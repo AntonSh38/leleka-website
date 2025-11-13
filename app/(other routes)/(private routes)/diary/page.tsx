a34856bd766cb810610668217bcb8e26a1461679
Roman Kniazhyk <romanknyazhyk@gmail.com>
2025-11-13 22:24:54 +0200

    Add text on empty note

--- app/(other routes)/(private routes)/diary/page.tsx
+++ app/(other routes)/(private routes)/diary/page.tsx
@@ -5,9 +5,12 @@ import DiaryEntryDetails from "@/components/DiaryPage/DiaryEntryDetails/DiaryEnt
 import DiaryList from "@/components/DiaryPage/DiaryList/DiaryList";
 import css from "./DiaryPage.module.css";
 import { useWindowSize } from "@/hooks/useWindowsSize";
+import { useSelectedNoteStore } from "@/lib/store/selectedNoteStore";
 
 export default function JourneyPage() {
   const { width } = useWindowSize();
+  const selectedNote = useSelectedNoteStore((s) => s.selectedNote);
+  console.log(selectedNote);
   return (
     <div>
       <GreetingBlock />
@@ -15,6 +18,11 @@ export default function JourneyPage() {
         <DiaryList />
 
         {width >= 1440 ? <DiaryEntryDetails /> : null}
+        {!selectedNote && width >= 1440 && (
+          <p className={css.diary_template}>
+            Оберіть нотатку для перегляду деталей
+          </p>
+        )}
       </div>
     </div>
   );
